d27943c04468cf62042205d2f45287924a47f204
20wontons <nbngusa@yahoo.com>
2022-01-31 16:02:08 -0800

    User can compose a new Tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -1,8 +1,12 @@
 package com.codepath.apps.restclienttemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -69,6 +73,29 @@ class TimelineActivity : AppCompatActivity() {
         }
 
         rvTweets.addOnScrollListener(scrollListener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.compose) {
+            val intent = Intent(this, ComposeActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            val tweet = data?.getParcelableExtra("tweet") as Tweet
+            tweets.add(0,tweet)
+            adapter.notifyItemInserted(0)
+            rvTweets.smoothScrollToPosition(0)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
     fun populateHomeTimeline() {
@@ -140,5 +167,6 @@ class TimelineActivity : AppCompatActivity() {
 
     companion object {
         const val TAG = "TimelineActivity"
+        const val REQUEST_CODE = 20
     }
 }
